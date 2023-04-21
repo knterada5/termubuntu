ee9cd5c3f6d2d57c374ea563f20780f3816b1e94
knterada5@Rapport <knterada5@gmail.com>
2023-04-21 14:54:54 +0900

    yes

--- setting_dotfiles_termux.sh
+++ setting_dotfiles_termux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setting VNC.
-sudo -S apt install tigervnc-standalone-server openbox xterm
+sudo -S apt install tigervnc-standalone-server openbox xterm -y
 
 # Download dotfiles.
 git clone https://github.com/knterada5/.dotfiles.git
