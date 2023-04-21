79ed5fbf08aa26c9038b5869487b65092fc6afec
knterada5@Rapport <knterada5@gmail.com>
2023-04-21 14:43:34 +0900

    termux directory

--- start_termux.sh
+++ start_termux.sh
@@ -9,7 +9,7 @@ echo "# Termux bashrc" >> .bashrc
 # Install Ubuntu.
 pkg install proot proot-distro -y
 proot-distro install ubuntu
-proot-distro login ubuntu --termux-home -- $HOME/termubuntu/setting_user_ubuntu.sh
+proot-distro login ubuntu --termux-home -- bash termubuntu/setting_user_ubuntu.sh
 
 # Reload bashrc.
 shopt -s expand_aliases
