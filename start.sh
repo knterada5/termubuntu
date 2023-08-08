dba259f6618b21ffc636eb4579fd984c5bf41c50
knterada5@Rapport <knterada5@gmail.com>
2023-08-08 11:54:00 +0900

    yes/no

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pkg update -y && pkg upgrade -y
+pkg update -y && yes n | pkg upgrade -y
 pkg install git
 git clone https://github.com/knterada5/termubuntu.git $HOME/termubuntu
 bash $HOME/termubuntu/scrips/start_termux.sh
