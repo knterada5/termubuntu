e003c10723e309a22790d8185854adbfc2c07f2e
knterada5@Rapport <knterada5@gmail.com>
2023-08-08 12:27:18 +0900

    scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 yes n | pkg update -y && pkg upgrade -y
 pkg install git
 git clone https://github.com/knterada5/termubuntu.git $HOME/termubuntu
-bash $HOME/termubuntu/scrips/start_termux.sh
+bash $HOME/termubuntu/scripts/start_termux.sh
