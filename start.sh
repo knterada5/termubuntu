58c7c3a2834d26d55f5a06ca8e617a7cb63717c3
knterada5 <119117377+knterada5@users.noreply.github.com>
2023-08-09 18:34:08 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 
 yes n | pkg update -y && pkg upgrade -y
 pkg install git
-read -p "Enter User name: " NAME
 git clone https://github.com/knterada5/termubuntu.git $HOME/termubuntu
-bash $HOME/termubuntu/scripts/start_termux.sh $NAME
+bash $HOME/termubuntu/scripts/start_termux.sh
