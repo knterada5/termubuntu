cab57c4a96b214dd72a10bf3c6a91c38589f7d65
knterada5 <119117377+knterada5@users.noreply.github.com>
2023-08-10 13:45:18 +0900

    Update start_ubuntu.sh

--- scripts/start_ubuntu.sh
+++ scripts/start_ubuntu.sh
@@ -3,6 +3,11 @@
 # Setting VNC.
 sudo -S apt install tigervnc-standalone-server openbox xterm -y
 
+# Setting Time Zone
+ln -fs /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
 # Download dotfiles.
 read -n1 -p "Do you clone dotfiles? It will take a long time to set up. [y/N]: " yn
 if [[ $yn = [yY] ]]; then
