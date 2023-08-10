c421aaea4fa77ec1b7ba549b543cb661ae843691
knterada5 <119117377+knterada5@users.noreply.github.com>
2023-08-10 09:56:51 +0900

    Update start_ubuntu.sh

--- scripts/start_ubuntu.sh
+++ scripts/start_ubuntu.sh
@@ -7,4 +7,6 @@ sudo -S apt install tigervnc-standalone-server openbox xterm -y
 read -n1 -p "Do you clone dotfiles? It will take a long time to set up. [y/N]: " yn
 if [[ $yn = [yY] ]]; then
   curl -LO https://raw.githubusercontent.com/knterada5/.dotfiles/main/install_linux.sh; source install_linux.sh
+else
+  printf '\n'
 fi
